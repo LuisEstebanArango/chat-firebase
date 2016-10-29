1638cdbed77be0ad0c4dcd15ddd4dedee515907f
luisesteban <estebanarango14@hotmail.com>
2016-10-29 07:55:10 -0500

    Se adiciona routeprovider, run y chatController

--- web/scripts/app.js
+++ web/scripts/app.js
@@ -5,17 +5,70 @@
   var app = angular.module('chat', ['ngRoute']);
 
   app.config(function ($routeProvider) {
-
-      $routeProvider
-        .when('/', {
-          templateUrl: 'views/nick.html',
-          controller: 'MainController',
-          controllerAs: 'jj'
-        });
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/nick.html',
+        controller: 'NickController',
+        controllerAs: 'nickController'
+      })
+      .when('/chat', {
+        templateUrl: 'views/chat.html',
+        controller: 'ChatController',
+        controllerAs: 'chatController'
       });
+  });
 
-  app.controller('MainController', [function () {
-    var mainController = this;
+  app.run([function (){
+  }]);
+
+  app.controller('NickController', ['$location', '$rootScope', function ($location, $rootScope) {
+    var nickController = this;
+    nickController.nick = 'goku';
+    nickController.saveNick = saveNick;
+    activate();
+
+    function activate(){
+      $.material.init();
+    }
+
+
+    function saveNick(){
+      if( nickController.nick == '' || nickController.nick == undefined ){
+        swal("Oops...", "Something went wrong!", "error");
+      } else {
+        $rootScope.nick = nickController.nick;
+        $location.path('/chat');
+      }
+    }
+
+  }]);
+
+  app.controller('ChatController', ['$location', '$rootScope', function ($location, $rootScope) {
+    var chatController = this;
+    chatController.saveNick   = saveNick;
+    chatController.writePush  = writePush;
+    activate();
+
+    function activate(){
+      $.material.init();
+    }
+
+    function saveNick(){
+      if( chatController.nick == '' || chatController.nick == undefined ){
+        swal("Oops...", "Something went wrong!", "error");
+      } else {
+        $location.path('/chat');
+      }
+    }
+
+    function writePush(llave, valor){
+      firebase.database().ref().push({
+        msj: valor,
+        email: llave
+      });
+    }
+
+
   }]);
 
 
